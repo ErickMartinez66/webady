3b15a749442ffb94c12b5c1b982f024fe4a53293
ErickMartinez66 <em977537@gmail.com>
2025-12-12 18:16:24 +0100

    filtrados de templates de desodorante y perfumes

--- apps/newapp/views.py
+++ apps/newapp/views.py
@@ -7,20 +7,82 @@ def vistaprin(request):
 
 
 def vistaperfumes(request):
-    perfumes_list = perfumes.objects.all()
-    context = {
+    boton_pulsado = request.GET.get('boton',None)
+    
+    if boton_pulsado == 'todos':
+        perfumes_list = perfumes.objects.all()
+        context = {
         'perfumes': perfumes_list,
         'titulo': 'Nuestra Colección de Perfumes'
-    }
-    return render(request, 'perfumes.html', context)
+        }
+        return render(request, 'perfumes.html', context)
+    elif boton_pulsado == 'mujer':
+        perfumes_list = perfumes.objects.filter(genero='F')
+        context = {
+        'perfumes': perfumes_list,
+        'titulo': 'Nuestra Colección de Perfumes'
+        }
+        return render(request, 'perfumes.html', context)
+    elif boton_pulsado == 'hombre':
+        perfumes_list = perfumes.objects.filter(genero='M')
+        context = {
+        'perfumes': perfumes_list,
+        'titulo': 'Nuestra Colección de Perfumes'
+        }
+        return render(request, 'perfumes.html', context)
+    elif boton_pulsado == 'unisex':
+        perfumes_list = perfumes.objects.filter(genero='U')
+        context = {
+        'perfumes': perfumes_list,
+        'titulo': 'Nuestra Colección de Perfumes'
+        }
+        return render(request, 'perfumes.html', context)
+    else:
+        perfumes_list = perfumes.objects.all()
+        context = {
+            'perfumes': perfumes_list,
+            'titulo': 'Nuestra Colección de Perfumes'
+        }
+        return render(request, 'perfumes.html', context)
 
 def vistadesodo(request):
-    desodo_list = desodorantes.objects.all()
-    context = {
+    boton_pulsado = request.GET.get('boton',None)
+    
+    if boton_pulsado == 'todos':
+        desodo_list = desodorantes.objects.all()
+        context = {
         'desodorantes': desodo_list,
-        'titulo': 'Nuestra Colección de desodorantes'
-    }
-    return render(request, 'desodorantes.html', context)
+        'titulo': 'Nuestra Colección de Desodorantes'
+        }
+        return render(request, 'desodorantes.html', context)
+    elif boton_pulsado == 'mujer':
+        desodo_list = desodorantes.objects.filter(genero='F')
+        context = {
+        'desodorantes': desodo_list,
+        'titulo': 'Nuestra Colección de Desodorantes'
+        }
+        return render(request, 'desodorantes.html', context)
+    elif boton_pulsado == 'hombre':
+        desodo_list = desodorantes.objects.filter(genero='M')
+        context = {
+        'desodorantes': desodo_list,
+        'titulo': 'Nuestra Colección de Desodorantes'
+        }
+        return render(request, 'desodorantes.html', context)
+    elif boton_pulsado == 'unisex':
+        desodo_list = desodorantes.objects.filter(genero='U')
+        context = {
+        'desodorantes': desodo_list,
+        'titulo': 'Nuestra Colección de Desodorantes'
+        }
+        return render(request, 'desodorantes.html', context)
+    else:
+        desodo_list = desodorantes.objects.all()
+        context = {
+            'desodorantes': desodo_list,
+            'titulo': 'Nuestra Colección de Desodorantes'
+        }
+        return render(request, 'desodorantes.html', context)
 
 def vistacremas(request):
     cremas_list = cremas.objects.all()
